d0771874e2d16a245050eb2a2e98b2da279b4ff6
felipekucaniz <felipekucaniz@gmail.com>
2024-10-04 03:23:10 -0300

    Update featured-product.js

--- assets/featured-product.js
+++ assets/featured-product.js
@@ -6,8 +6,9 @@ document.addEventListener('DOMContentLoaded', () => {
       .then((data) => {
         for (i = 0; i < data.items.length; i++) {
           if (data.items[i].id == variantSelector.value) {
-            if (document.querySelector("button[name='add']").innerText == 'Add to Cart') {
-              document.querySelector("button[name='add']").innerText = 'Added to Cart';
+            console.log(document.querySelector("button[name='add']").innerText);
+            if (document.querySelector("button[name='add']").innerText == 'ADD TO CART') {
+              document.querySelector("button[name='add']").innerText = 'ADDED TO CART';
             }
           }
         }
