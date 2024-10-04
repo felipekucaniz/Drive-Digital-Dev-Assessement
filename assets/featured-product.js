22db901196df5565d4d5f0db21bd4139c78d0438
felipekucaniz <felipekucaniz@gmail.com>
2024-10-04 03:19:10 -0300

    Update featured-product.js
    Change button text when variant change and item is already in cart

--- assets/featured-product.js
+++ assets/featured-product.js
@@ -1,6 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
   var variantSelector = document.querySelector("select[name='id']");
   variantSelector.addEventListener('change', function () {
+    var cartContents = fetch(window.Shopify.routes.root + 'cart.js')
+      .then((response) => response.json())
+      .then((data) => {
+        for (i = 0; i < data.items.length; i++) {
+          if (data.items[i].id == variantSelector.value) {
+            if (document.querySelector("button[name='add']").innerText == 'Add to Cart') {
+              document.querySelector("button[name='add']").innerText = 'Added to Cart';
+            }
+          }
+        }
+      });
+    // console.log(cartContents);
     // Update DOM price element
     // console.log(variantSelector.options[variantSelector.selectedIndex].dataset.price);
     document.getElementById('price-tag').innerText =
